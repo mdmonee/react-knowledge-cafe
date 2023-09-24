f85039b42e749e671faf3120501852718d371030
mdmonee <monirulislamnrl1@gmail.com>
2023-09-25 04:46:19 +0600

    api created

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Header from './Component/Header/Header'
+import Blogs from './Component/Blogs/Blogs'
+
+function App() {
+
+  return (
+    <>
+     
+      <Header></Header>
+      <Blogs></Blogs>
+    
+    </>
+  )
+}
+
+export default App
